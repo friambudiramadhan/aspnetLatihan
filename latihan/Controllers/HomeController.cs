68103b48eaaf9ca5326ab6b6426ef8036e841b19
Friambudi <sfriambudi@gmail.com>
2022-08-09 21:54:23 +0700

    First commit

---
+++ latihan/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using latihan.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using latihan.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace latihan.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SqlConnection _conn;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _conn = new SqlConnection(config.GetConnectionString("default"));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ latihan/Models/StudentsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace latihan.Models
+{
+    public class StudentsModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public int company_id { get; set; }
+    }
+}
